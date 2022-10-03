0488964b5b6d76bca53006ef70e8609bbe99b478
joung45387 <joung45387@gmail.com>
2022-10-03 15:13:31 +0900

    Initial commit

---
+++ src/main/java/com/everyAuction/everyAuction/EveryAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.everyAuction.everyAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everyAuction/everyAuction/EveryAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyAuction.everyAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
